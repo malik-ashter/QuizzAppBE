2351e668bbb79f3c3b15b2e72831e82c4995f6cd
Malik Ashter <malik.ashter@finksolution.it>
2023-01-22 22:18:48 +0100

    hide answers

--- routes/questions.js
+++ routes/questions.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/ur', (req, res)=>{
+    if(!(questionsUrdu[0].showAnswers == 'yes')) {
+        questionsUrdu.forEach(element => {
+            delete element["answer"];
+        });
+    }
     res.send(questionsUrdu);
 });
 
 router.get('/en', (req, res)=>{
+    if(!(questionsEnglish[0].showAnswers == 'yes')) {
+        questionsEnglish.forEach(element => {
+            delete element["answer"];
+        });
+    }
     res.send(questionsEnglish);
 });
 
--- routes/titles.js
+++ routes/titles.js
@@ -4,14 +4,16 @@ const router = express.Router();
 router.get('/', (req, res)=>{
     let titles = {
         titleEnglish: questionsEnglish[0].title,
-        titleUrdu: questionsUrdu[0].title
+        titleUrdu: questionsUrdu[0].title,
+        showAnswers: questionsEnglish[0].showAnswers
     };
     res.send(titles);
 });
 
 router.get('/en', (req, res)=>{
     let title = {
-        titleEnglish: questionsEnglish[0].title
+        titleEnglish: questionsEnglish[0].title,
+        showAnswers: questionsEnglish[0].showAnswers
     };
     console.log(titleEnglish);
     res.send(title);
