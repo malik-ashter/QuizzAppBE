924425ec161688a0dec6b7c0094e19ff16be004f
Malik Ashter <malik.ashter@finksolution.it>
2023-02-19 16:06:38 +0100

    cyclic readonly error, log to console only

--- utils/logger.js
+++ utils/logger.js
@@ -1,21 +1,24 @@
-const { format, createLogger, transports, winston } = require("winston");
-const { combine, timestamp, label, printf } = format;
-require("winston-daily-rotate-file");
+const winston = require('winston');
+// Define your severity levels.
+// With them, You can create log files,
+// see or hide levels based on the running ENV.
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+}
 
-//Label
-const CATEGORY = "Log Rotation";
-
-//DailyRotateFile func()
-const fileRotateTransport = new transports.DailyRotateFile({
-  filename: "logs/rotate-%DATE%.log",
-  datePattern: "YYYY-MM-DD",
-  maxFiles: "14d",
-});
-
-//Using the printf format.
-const customFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
-});
+// This method set the current severity based on
+// the current NODE_ENV: show all the log levels
+// if the server was run in development mode; otherwise,
+// if it was run in production, show only warn and error messages.
+const level = () => {
+  const env = process.env.NODE_ENV || 'development'
+  const isDevelopment = env === 'development'
+  return isDevelopment ? 'debug' : 'warn'
+}
 
 // Define different colors for each level.
 // Colors make the log message more visible,
@@ -25,16 +28,47 @@ const colors = {
   warn: 'yellow',
   info: 'green',
   http: 'magenta',
+  debug: 'white',
 }
 
 // Tell winston that you want to link the colors
 // defined above to the severity levels.
-// winston.addColors(colors)
+winston.addColors(colors)
 
-const logger = createLogger({
-  level: "debug",
-  format: combine(label({ label: CATEGORY }), timestamp(), customFormat),
-  transports: [fileRotateTransport, new transports.Console()],
-});
+// Chose the aspect of your log customizing the log format.
+const format = winston.format.combine(
+  // Add the message timestamp with the preferred format
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+  // Tell Winston that the logs must be colored
+  winston.format.colorize({ all: true }),
+  // Define the format of the message showing the timestamp, the level and the message
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+  ),
+)
 
-module.exports = logger;
+// Define which transports the logger must use to print out messages.
+// In this example, we are using three different transports
+const transports = [
+  // Allow the use the console to print the messages
+  new winston.transports.Console(),
+  // Allow to print all the error level messages inside the error.log file
+  // new winston.transports.File({
+  //   filename: 'logs/error.log',
+  //   level: 'error',
+  // }),
+  // Allow to print all the error message inside the all.log file
+  // (also the error log that are also printed inside the error.log(
+  // new winston.transports.File({ filename: 'logs/all.log' }),
+]
+
+// Create the logger instance that has to be exported
+// and used to log messages.
+const logger = winston.createLogger({
+  level: level(),
+  levels,
+  format,
+  transports,
+})
+
+module.exports = logger
